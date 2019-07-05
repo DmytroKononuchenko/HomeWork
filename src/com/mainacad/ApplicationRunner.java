d2ea51ed66e6a07cc39bb95d3104b30b27cde03b
Дмитро <konuk008@ukr.net>
2019-07-05 08:29:00 +0300

    Add Runner class

---
+++ src/com/mainacad/ApplicationRunner.java
@@ -0,0 +1,38 @@
+package com.mainacad;
+
+import com.mainacad.model.*;
+
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(10);
+
+
+        Square square = new Square(15);
+
+
+        Triangle triangle = new Triangle(12, 15);
+
+        Quadrilateral quadrilateral = new Quadrilateral(100,200);
+
+        Shape maxShape;
+
+        if (circle.getArea() > square.getArea()) {
+            maxShape = circle;
+        }
+        else {
+            maxShape = square;
+        }
+
+        if (maxShape.getArea() < triangle.getArea()){
+            maxShape = triangle;
+        }
+
+        if (maxShape.getArea() < quadrilateral.getArea()){
+            maxShape = quadrilateral;
+        }
+
+        System.out.println("Max shape is " + maxShape.getClass().getSimpleName() + " and has area " + maxShape.getArea());
+    }
+}
